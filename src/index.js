7aa3386e28e7bf25c9f781858e06aa1d7ce58d5b
Harish Sivakumaran <harish.sivakumaran@sap.com>
2022-05-06 18:28:37 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -3,15 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>
 );
+
+function Home(){
+  return (
+    <div>
+      <h1> Team 1907 </h1>
+      <a>run app</a>
+    </div>
+  );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals((oResp) => console.log(oResp));
